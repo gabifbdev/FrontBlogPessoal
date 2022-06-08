6c7a095ba6e363dd014bc854155f44f39eee2bc9
Gabriela Felix <99824573+gabifbdev@users.noreply.github.com>
2022-06-08 18:46:48 -0300

    Implementação do put e delete de temas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,16 +4,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { TemaComponent } from './tema/tema.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
+import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 
 const routes: Routes = [
-{path: "", redirectTo: "entrar", pathMatch: "full"},
+{path: '', redirectTo: "entrar", pathMatch: "full"},
 {path:"entrar", component: EntrarComponent},
 {path:"cadastrar", component: CadastrarComponent},
 
 
 
 {path: 'inicio', component: InicioComponent},
-{path: 'tema',component: TemaComponent}
+{path: 'tema',component: TemaComponent},
+
+{path: 'tema-edit/:id', component: TemaEditComponent},
+
+{path: 'tema-delete/:id', component: TemaDeleteComponent}
 
 ];
 
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -15,11 +15,11 @@ export class AuthService {
   ) { }
 
   entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
-    return this.http.post<UsuarioLogin>('https://bloggerpessoal.herokuapp.com/usuarios/logar', usuarioLogin)
+    return this.http.post<UsuarioLogin>('https://blogpessoalthiago.herokuapp.com/usuarios/logar', usuarioLogin)
   }
 
   cadastrar(usuario: Usuario): Observable<Usuario>{
-    return this.http.post<Usuario>('https://bloggerpessoal.herokuapp.com/usuarios/cadastrar', usuario)
+    return this.http.post<Usuario>('https://blogpessoalthiago.herokuapp.com/usuarios/cadastrar', usuario)
   }
 
 
